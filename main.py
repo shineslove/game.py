25bbbe7550f039dab5a2811049d71d0068ab6d61
shineslove <unconquerablerivers@gmail.com>
2025-01-03 11:22:17 +0900

    updating the tiles to have black between them

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ pygame.init()
 
 FPS = 30
 SCREEN_SIZE = 500
-SCREEN_OFFSET = 1.55
+SCREEN_OFFSET = 1.45
 SCREEN_WIDTH = SCREEN_SIZE * SCREEN_OFFSET
 SCREEN_HEIGHT = SCREEN_SIZE * SCREEN_OFFSET
 
 WOODBROWN = (198, 152, 116)
 
 WHITE = (255, 255, 255)
+
+BLACK = (0, 0, 0)
 
 DISPLAYSCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -24,7 +26,7 @@ pygame.display.set_caption("Gungi")
 frames = pygame.time.Clock()
 
 SIZE = 50
-OFFSET = 10
+OFFSET = 5
 UPSCALE = 1.5
 DIMENSION = 9
 
@@ -35,12 +37,22 @@ while True:
             sys.exit()
     x, y = OFFSET, OFFSET
     width, height = SIZE * UPSCALE, SIZE * UPSCALE
+    pygame.draw.rect(
+        DISPLAYSCREEN,
+        BLACK,
+        (x * 0.85, y * 0.85, SCREEN_WIDTH * 0.99, SCREEN_HEIGHT * 0.99),
+    )
     for row in range(DIMENSION):
         for cell in range(DIMENSION):
             pygame.draw.rect(
                 DISPLAYSCREEN,
                 WHITE,
-                (x + row * (width + OFFSET), y + cell * (width + OFFSET), width, height),
+                (
+                    x + row * (width + OFFSET),
+                    y + cell * (width + OFFSET),
+                    width,
+                    height,
+                ),
             )
     pygame.display.update()
     frames.tick(FPS)
